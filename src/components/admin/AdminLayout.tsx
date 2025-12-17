7b765d8d450abf815819608fa9c069fccbb0521a
Anh Quang Vũ <23a1001d0025@students.hou.edu.vn>
2025-12-17 15:49:27 +0700

    fix: comment out notification button in AdminLayout component

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -116,10 +116,10 @@ export default function AdminLayout({ user, onLogout }: AdminLayoutProps) {
             </div>
 
             <div className="flex items-center space-x-4 ml-4">
-              <button className="relative p-2 text-gray-600 hover:bg-gray-100 rounded-lg">
+              {/* <button className="relative p-2 text-gray-600 hover:bg-gray-100 rounded-lg">
                 <Bell className="w-6 h-6" />
                 <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
-              </button>
+              </button> */}
             </div>
           </div>
         </header>
